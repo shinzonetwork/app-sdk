a188ad934a955fa00821eedb31bba2ac77984bff
Quinn Purdy <quinn.purdy.gamedev@gmail.com>
2025-11-24 15:18:28 -0500

    Use defra keychain

--- pkg/defra/defra_test.go
+++ pkg/defra/defra_test.go
@@ -26,10 +26,32 @@ func TestStartDefraUsingConfig(t *testing.T) {
 	testConfig, err := config.LoadConfig(configPath)
 	require.NoError(t, err)
 
-	testConfig.DefraDB.Url = "127.0.0.1:0" // In case we have something else running
+	testConfig.DefraDB.Url = "127.0.0.1:0"      // In case we have something else running
 	testConfig.DefraDB.Store.Path = t.TempDir() // Use isolated temp directory for each test
 
 	myNode, err := StartDefraInstance(testConfig, &MockSchemaApplierThatSucceeds{})
 	require.NoError(t, err)
 	myNode.Close(context.Background())
+}
+
+func TestSubsequentRestartsYieldTheSameIdentity(t *testing.T) {
+	testConfig := DefaultConfig
+	testConfig.DefraDB.KeyringSecret = "testSecret"
+	myNode, err := StartDefraInstance(testConfig, &MockSchemaApplierThatSucceeds{})
+	require.NoError(t, err)
+
+	peerInfo, err := myNode.DB.PeerInfo()
+	require.NoError(t, err)
+	require.NotNil(t, peerInfo)
+	require.Greater(t, len(peerInfo), 0)
+
+	err = myNode.Close(t.Context())
+	require.NoError(t, err)
+
+	myNode, err = StartDefraInstance(testConfig, &MockSchemaApplierThatSucceeds{})
+	require.NoError(t, err)
+
+	newPeerInfo, err := myNode.DB.PeerInfo()
+	require.NoError(t, err)
+	require.ElementsMatch(t, peerInfo, newPeerInfo)
 }
