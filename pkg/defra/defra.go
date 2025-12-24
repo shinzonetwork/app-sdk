9b17a5cd72be7e9a8105b64d65a98778e99c7b6d
dvncan <dunbrown64@gmail.com>
2025-12-24 13:34:46 -0500

    chore: remove hardcoded logger to pass a path on init chore: update all init locations to use new pattern

--- pkg/defra/defra.go
+++ pkg/defra/defra.go
@@ -46,6 +46,7 @@ var DefaultConfig *config.Config = &config.Config{
 	},
 	Logger: config.LoggerConfig{
 		Development: false,
+		LogsDir:     "./logs",
 	},
 }
 
@@ -322,7 +323,7 @@ func StartDefraInstance(cfg *config.Config, schemaApplier SchemaApplier, collect
 		cfg.DefraDB.P2P.ListenAddr = defaultListenAddress
 	}
 
-	logger.Init(cfg.Logger.Development)
+	logger.Init(cfg.Logger.Development, cfg.Logger.LogsDir)
 
 	// Use persistent identity from keyring (required, no fallback)
 	nodeIdentity, err := getOrCreateNodeIdentity(cfg)
